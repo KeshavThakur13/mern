035ee73830ea6440ff803f0887c2ec3bb0e9045a
Keshav Kumar Thakur <keshav.thakur.58679@gmail.com>
2024-08-03 00:48:22 +0545

    second

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import http from "http";
 const port = process.env.PORT||8000;
 const server = http.createServer((req,res)=>{
     if(req.url==="/"){
-        res.end("<h1>Home Page</h1>");
+        res.end("<h1>This is Home Page</h1>");
     }
     else if(req.url==="/about"){
-        res.end("<h1>About Us Page</h1>");
+        res.end("<h1>This is About Us Page</h1>");
     }
     else{
         res.end("<h1>404 Page Not Found</h1>");
